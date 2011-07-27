dadbae438f3892dce581952f99a9bbef2d767797
Crane <szcshui@gmail.com>
2011-07-26 20:29:33 -0500

    update datastore test

---
+++ deploy.sh
@@ -0,0 +1,111 @@
+#!/bin/bash
+
+server=${1-localhost:8085}
+
+function clearup{
+	kill $server_pid
+	rm gradle.properties
+}
+
+term "cleanup" INT TERM EXIT
+
+function unknown_files {
+	unknown_file_count=`git status --porcelain | grep "^??" | wc -l`
+	[[ "$unknown_file_count" -gt 0 ]]
+}
+
+function uncommited_changes_ {
+	changes_commited=`git diff HEAD --shortstat | wc -l `
+	[[ "$changes_commited" -gt 0 ]]
+}
+
+if unknown_files; then
+	echo 'unknown files in project'
+	exit 1
+fi
+
+if uncommited_changes_; then
+	echo 'Uncommited files in project'
+	exit 1
+fi 
+
+gradle clean build 
+if [ "$?" -gt 0 ] ; then
+	exit 1
+fi
+
+gradle gaeRun &
+server_pid=$!
+if [ "$server_pid" -gt 0 ] ; then
+	echo "Server failed to start"
+	exit 1
+fi
+
+echo -n "Waiting for local server to start..."
+server_status=1
+while [ ! "$server_status" -gt 0 ] ; do
+	echo -n .
+	echo http://$server/checkclearing
+	server_status=$?
+	sleep 1
+done
+
+
+curl -s -H 'Content-Type:application/json' -d '["one"]' http://$server/checkclearing
+
+history=`curl http://$server/checkclearing`
+
+echo 'history data:'
+echo $history
+
+echo $history | python -mjson.tool > /dev/null
+if [ "$?" -eq 0 ]; then
+	echo "Response is valid JSON"
+else
+	echo "Response is NOT valid JSON"
+	echo $history
+	kill $server_pid
+	exit 1	
+fi
+
+kill $server_pid
+
+echo "Build successful! Enter AppEnigner password to deploy"
+stty -echo
+read -p "Password: " password
+echo
+stty echo
+echo "gaePassword=$password" > gradle.properties
+#! gradle gaeUpload
+rm gradle.properties
+
+
+timestamp=`date "+%Y-%m-%d %H:%M:%S"`
+git tag DEPLOY_$timestamp
+echo $timestamp
+
+
+echo 'exit...'
+exit 0
+
+
+response=`curl -H 'Content-Type:application/json' -d "$history" http://$server/checkclearing`
+
+echo $response
+
+if [ "$response" != '{"one":100}' ]; then
+
+		echo "Test Failed!!"
+		exit 1
+else
+	echo "Test OK"
+fi
+
+
+
+
+
+
+
+
+
---
+++ src/test/java/edu/luc/clearing/DatastoreAdapterTest.java
@@ -0,0 +1,74 @@
+package edu.luc.clearing;
+
+import static org.mockito.Mockito.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
+import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
+import com.google.apphosting.api.ApiProxy;
+
+import com.google.appengine.tools.development.ApiProxyLocal;
+import com.google.appengine.tools.development.ApiProxyLocalFactory;
+import com.google.appengine.tools.development.AbstractLocalRpcService;
+import com.google.appengine.api.datastore.AsyncDatastoreService; 
+
+public class DatastoreAdapterTest {
+
+	// in order to avoid exception:
+	// java.lang.NullPointerException: No API environment is registered for this
+	// thread.
+	// at
+	// com.google.appengine.api.datastore.DatastoreApiHelper.getCurrentAppId(DatastoreApiHelper.java:108)
+	// at
+	// com.google.appengine.api.datastore.DatastoreApiHelper.getCurrentAppIdNamespace(DatastoreApiHelper.java:118)
+	// at com.google.appengine.api.datastore.Query.<init>(Query.java:112)
+	// at edu.luc.clearing.DatastoreAdapter.runQuery(DatastoreAdapter.java:29)
+	//
+	// link:
+	// http://code.google.com/appengine/docs/java/tools/localunittesting.html#Introducing_the_Java_Testing_Utilities
+
+	private final LocalServiceTestHelper helper = new LocalServiceTestHelper(
+			new LocalDatastoreServiceTestConfig().setDefaultHighRepJobPolicyUnappliedJobPercentage(100));
+
+
+	@Before
+	public void setup(){
+		helper.setUp();
+		
+		//ApiProxyLocal proxy = (ApiProxyLocal) ApiProxy.getDelegate();
+		//proxy.setProperty(LocalDatastoreService.NO_STORAGE_PROPERTY, Boolean.TRUE.toString());
+	//	ApiProxy.setEnvironmentForCurrentThread(new TestEnvironment()); 
+	//	  ApiProxyLocalImpl proxy = (ApiProxyLocalImpl)
+	//			  ApiProxy.getDelegate();
+	//			          proxy.setProperty(LocalDatastoreService.NO_STORAGE_PROPERTY,
+	//			  Boolean.TRUE.toString()); 
+	}
+	
+    @After
+    public void tearDown() {
+        //helper.tearDown();
+    }
+    
+    
+	@Test
+	public void canSaveAmounts() throws Exception {
+		DatastoreService googleStore = mock(DatastoreService.class);
+		//DatastoreService googleStore = DatastoreServiceFactory.getDatastoreService();
+		
+		DatastoreAdapter store = new DatastoreAdapter(googleStore);
+		store.saveRow("Amount", "one");
+		verify(googleStore).put((Entity) anyObject());
+	}
+	
+	@Test
+	public void canQuerySomething() throws Exception {
+		//TODO:
+		
+	}
+}
